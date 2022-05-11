672f07f73bafef97ab6262529f4fcb0c0076f4e7
CY-9497 <cc.chen.chen@foxmail.com>
2022-05-11 18:16:24 +0800

    Initial commit

---
+++ eureka-server/src/main/java/cn/itcast/eureka/EurekaApplication.java
@@ -0,0 +1,17 @@
+package cn.itcast.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author 渔郎
+ * @Date 2022/5/11 11:50
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
