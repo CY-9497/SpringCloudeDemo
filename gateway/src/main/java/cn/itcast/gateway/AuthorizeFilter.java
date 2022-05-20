c3a4be3e6c3b7fae20e041db3552b7b0799ff64d
CY-9497 <cc.chen.chen@foxmail.com>
2022-05-20 18:06:07 +0800

    commit

---
+++ gateway/src/main/java/cn/itcast/gateway/AuthorizeFilter.java
@@ -0,0 +1,37 @@
+package cn.itcast.gateway;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * @author 渔郎
+ * @Date 2022/5/20 17:47
+ */
+@Order(-1)
+//@Component
+public class AuthorizeFilter implements GlobalFilter {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        //获取请求参数
+        ServerHttpRequest request = exchange.getRequest();
+        MultiValueMap<String, String> params = request.getQueryParams();
+        //获取参数中authorization参数
+        String auth = params.getFirst("authorization");
+        //判断参数值是否符合admin
+        if ("admin".equals(auth)){
+            //是，放行
+            return chain.filter(exchange);
+        }
+        //否，拦截
+        //设置状态码
+        exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+        return exchange.getResponse().setComplete();
+    }
+}
---
+++ gateway/src/main/java/cn/itcast/gateway/GatewayApplication.java
@@ -0,0 +1,15 @@
+package cn.itcast.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 渔郎
+ * @Date 2022/5/20 17:00
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
