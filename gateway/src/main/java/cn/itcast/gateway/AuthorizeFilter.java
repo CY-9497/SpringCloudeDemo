ca510dabdb16016f8ad054739bf1eadd6f0c58c2
CY-9497 <cc.chen.chen@foxmail.com>
2022-05-26 22:00:18 +0800

    commit

--- gateway/src/main/java/cn/itcast/gateway/AuthorizeFilter.java
+++ gateway/src/main/java/cn/itcast/gateway/AuthorizeFilter.java
@@ -5,7 +5,6 @@ import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
-import org.springframework.stereotype.Component;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
